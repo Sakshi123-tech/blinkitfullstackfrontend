e39aee1f5eec310aa1d5d843b008c5e28b84b570
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-07-03 04:05:08 +0530

    Update SummaryApi.js

--- src/common/SummaryApi.js
+++ src/common/SummaryApi.js
@@ -1,5 +1,4 @@
-export const baseURL = import.meta.env.VITE_API_BASE_URL;
-
+export const baseURL = "https://binkitfullstackbackend-2.onrender.com/"
 const SummaryApi = {
     register : {
         url : '/api/user/register',
